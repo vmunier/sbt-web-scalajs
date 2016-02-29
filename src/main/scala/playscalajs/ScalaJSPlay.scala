4d45be91f14dfb5f59d89f73fb13e54a39b27b41
Vincent Munier <vmunier.fr@gmail.com>
2016-02-29 16:11:16 +0000

    Remove deprecated settings in ScalaJSPlay.

--- src/main/scala/playscalajs/ScalaJSPlay.scala
+++ src/main/scala/playscalajs/ScalaJSPlay.scala
@@ -14,20 +14,10 @@ object ScalaJSPlay extends AutoPlugin {
 
   object autoImport {
     val sourceMappings = Def.settingKey[Seq[(File, String)]]("Mappings of files to their hashed canonical path")
-
-    @Deprecated
-    val mapSourceURI = Def.settingKey[Option[String]]("Scalac option value to rewrite source URIs inside Scala.js Source Maps")
-    @Deprecated
-    val sourceMapsBase = Def.settingKey[File]("Source Maps base directory")
-    @Deprecated
-    val sourceMapsDirectories = Def.settingKey[Seq[File]]("Directories containing the Scala files needed by Source Maps")
   }
   import autoImport._
 
   override def projectSettings: Seq[Setting[_]] = Seq(
-    mapSourceURI := None,
-    sourceMapsBase := baseDirectory.value,
-    sourceMapsDirectories := Seq(sourceMapsBase.value),
     emitSourceMaps in fullOptJS := false,
     sourceMappings := SourceMappings.fromFiles((unmanagedSourceDirectories in Compile).value),
     scalacOptions ++= sourceMappingOptions(sourceMappings.value)
