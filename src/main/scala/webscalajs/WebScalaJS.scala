50fef7d56ba4d14804392fa0d5685ec3fff2a932
Vincent Munier <vmunier.fr@gmail.com>
2020-02-09 22:44:09 +0000

    Fix scope when using scalaJSLinkerConfig.

--- src/main/scala/webscalajs/WebScalaJS.scala
+++ src/main/scala/webscalajs/WebScalaJS.scala
@@ -3,7 +3,6 @@ package webscalajs
 import com.typesafe.sbt.web.SbtWeb.autoImport._
 import com.typesafe.sbt.web.pipeline.Pipeline
 import com.typesafe.sbt.web.{PathMapping, SbtWeb}
-import org.scalajs.jsdependencies.sbtplugin.JSDependenciesPlugin.autoImport._
 import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport._
 import sbt.Def.Initialize
 import sbt.Keys._
@@ -147,7 +146,7 @@ object WebScalaJS extends AutoPlugin {
   def sourcemapScalaFiles(optJS: TaskKey[Attributed[File]]): Initialize[Task[Seq[PathMapping]]] = Def.taskDyn {
     val projectsWithSourceMaps = filterInitializeSeq(
       scalaJSProjects,
-      (p: Project) => Def.setting(scalaJSLinkerConfig.in(p, optJS).value.sourceMap)).value
+      (p: Project) => Def.setting(scalaJSLinkerConfig.in(p, Compile, optJS).value.sourceMap)).value
 
     Def.task {
       val sourceDirectories = settingOnProjects(transitiveDependencies(projectsWithSourceMaps), unmanagedSourceDirectories).value
