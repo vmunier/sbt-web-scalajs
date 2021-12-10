a002c52e2f985b22c9855b79f7aa74b2bf98b2a5
slovic <slovic.r@gmail.com>
2021-12-10 14:26:22 +0100

    set scalaJSStage per configuration
    Allow to set scalaJSStage per configuration e.g.:
    
    ```scala
    //in scalajs project
    Debian / scalaJSStage := FullOptStage
    ```

--- src/main/scala/webscalajs/ScalaJSStageTasks.scala
+++ src/main/scala/webscalajs/ScalaJSStageTasks.scala
@@ -2,13 +2,14 @@ package webscalajs
 import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport._
 import org.scalajs.sbtplugin.Stage
 import sbt.Def.Initialize
+import sbt.Keys.configuration
 import sbt._
 
 private[webscalajs] object ScalaJSStageTasks {
 
   def onScalaJSStage[A](onFastOpt: => Initialize[A], onFullOpt: => Initialize[A]): Initialize[A] =
     Def.settingDyn {
-      scalaJSStage.value match {
+      (configuration / scalaJSStage).value match {
         case Stage.FastOpt => onFastOpt
         case Stage.FullOpt => onFullOpt
       }
